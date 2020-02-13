a40e75a8bd93f64d4d7fbf86d8d153393645d672
Alex <quacksterr.15@gmail.com>
2020-02-13 10:20:47 +1000

    Update Elias.cs

--- Elias/Elias.cs
+++ Elias/Elias.cs
@@ -35,6 +35,16 @@ namespace EliasLibrary
             get { return Path.Combine(CAST_PATH, "images"); }
         }
 
+        public string JPEXES_DIR1
+        {
+            get { return Path.Combine(OUTPUT_PATH, "images"); }
+        }
+
+        public string JPEXES_DIR2
+        {
+            get { return Path.Combine(OUTPUT_PATH, "binaryData"); }
+        }
+
         private List<EliasAsset> Assets;
 
         public Elias(string sprite, bool IsSmallFurni, string fileName, int X, int Y, string FFDEC_PATH, string OUTPUT_PATH, string DIRECTOR_PATH)
@@ -100,6 +110,12 @@ namespace EliasLibrary
 
         private void RunEliasDirector()
         {
+            try
+            {
+                Directory.Delete(Path.Combine(OUTPUT_PATH, "images"), true);
+                Directory.Delete(Path.Combine(OUTPUT_PATH, "binaryData"), true);
+            } catch { }
+
             var p = new Process();
             p.StartInfo.WorkingDirectory = new FileInfo(DIRECTOR_PATH).DirectoryName;
             p.StartInfo.FileName = DIRECTOR_PATH;
