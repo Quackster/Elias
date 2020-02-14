285f90971dbe3b27de170cec0eb951bea26320e7
Alex <quacksterr.15@gmail.com>
2020-02-14 13:38:36 +1000

    Update EliasAsset.cs

--- Elias/EliasAsset.cs
+++ Elias/EliasAsset.cs
@@ -11,8 +11,8 @@ namespace EliasLibrary
 {
     public class EliasAsset
     {
-        private Elias elias;
-        private XmlNode node;
+        private Elias Elias;
+        private XmlNode Node;
 
         public bool IsIcon;
         public bool IsShadow;
@@ -30,8 +30,8 @@ namespace EliasLibrary
 
         public EliasAsset(Elias elias, XmlNode node)
         {
-            this.elias = elias;
-            this.node = node;
+            this.Elias = elias;
+            this.Node = node;
             this.FlashRectanglePoint = new int[2];
             this.ShockwaveRectanglePoint = new int[2];
 
@@ -57,8 +57,8 @@ namespace EliasLibrary
                 }
             }
 
-            this.ParseAssetNames();
-            this.ParseRecPointNames();
+            ParseAssetNames();
+            ParseRecPointNames();
         }
 
         public void ParseAssetNames()
@@ -69,9 +69,9 @@ namespace EliasLibrary
             if (IsShadow)
                 return;
 
-            for (int i = 0; i < node.Attributes.Count; i++)
+            for (int i = 0; i < Node.Attributes.Count; i++)
             {
-                var attribute = node.Attributes.Item(i);
+                var attribute = Node.Attributes.Item(i);
 
                 if (attribute == null)
                 {
@@ -80,16 +80,16 @@ namespace EliasLibrary
 
                 if (attribute.Name == "name")
                 {
-                    this.FlashAssetName = attribute.InnerText;
-                    this.ShockwaveAssetName = AssetUtil.ConvertFlashName(this.elias, attribute.InnerText, elias.X, elias.Y);
+                    FlashAssetName = attribute.InnerText;
+                    ShockwaveAssetName = AssetUtil.ConvertFlashName(Elias, attribute.InnerText, Elias.X, Elias.Y);
                 }
 
                 if (attribute.Name == "source")
                 {
-                    this.IsFlipped = true;
-                    this.IsMemberAlias = true;
-                    this.FlashSourceAliasName = attribute.InnerText;
-                    this.ShockwaveSourceAliasName = AssetUtil.ConvertFlashName(this.elias, attribute.InnerText, elias.X, elias.Y);
+                    IsFlipped = true;
+                    IsMemberAlias = true;
+                    FlashSourceAliasName = attribute.InnerText;
+                    ShockwaveSourceAliasName = AssetUtil.ConvertFlashName(Elias, attribute.InnerText, Elias.X, Elias.Y);
                 }
             }
         }
@@ -105,9 +105,9 @@ namespace EliasLibrary
             int x = -1;
             int y = -1;
 
-            for (int i = 0; i < node.Attributes.Count; i++)
+            for (int i = 0; i < Node.Attributes.Count; i++)
             {
-                var attribute = node.Attributes.Item(i);
+                var attribute = Node.Attributes.Item(i);
 
                 if (attribute == null)
                 {
@@ -123,13 +123,13 @@ namespace EliasLibrary
 
             if (x == -1 || y == -1)
             {
-                this.FlashRectanglePoint = new int[] { 0, 0 };
-                this.ShockwaveRectanglePoint = new int[] { 0, 0 };
+                FlashRectanglePoint = new int[] { 0, 0 };
+                ShockwaveRectanglePoint = new int[] { 0, 0 };
                 return;
             }
 
-            this.FlashRectanglePoint = new int[] { x, y };
-            this.ShockwaveRectanglePoint = new int[] { x - 32, y };
+            FlashRectanglePoint = new int[] { x, y };
+            ShockwaveRectanglePoint = new int[] { x - 32, y };
         }
 
         public void WriteAssets()
@@ -140,9 +140,9 @@ namespace EliasLibrary
             if (IsShadow)
                 return;
 
-            for (int i = 0; i < node.Attributes.Count; i++)
+            for (int i = 0; i < Node.Attributes.Count; i++)
             {
-                var attribute = node.Attributes.Item(i);
+                var attribute = Node.Attributes.Item(i);
 
                 if (attribute == null)
                 {
@@ -151,38 +151,38 @@ namespace EliasLibrary
 
                 if (attribute.Name == "name")
                 {
-                    this.FlashAssetName = attribute.InnerText;
-                    this.ShockwaveAssetName = AssetUtil.ConvertFlashName(this.elias, attribute.InnerText, elias.X, elias.Y);
+                    FlashAssetName = attribute.InnerText;
+                    ShockwaveAssetName = AssetUtil.ConvertFlashName(Elias, attribute.InnerText, Elias.X, Elias.Y);
 
-                    var flashFile = ImageAssetUtil.SolveFile(elias.OUTPUT_PATH, FlashAssetName);
+                    var flashFile = ImageAssetUtil.SolveFile(Elias.OUTPUT_PATH, FlashAssetName);
 
                     if (flashFile == null)
                     {
-                        var symbolData = ImageAssetUtil.SolveSymbolReference(elias, FlashAssetName);
+                        var symbolData = ImageAssetUtil.SolveSymbolReference(Elias, FlashAssetName);
 
                         if (symbolData != null)
                         {
                             var symbolID = symbolData.Item1;
                             var symbolReference = symbolData.Item2;
-                            var symbolFileName = Path.GetFileNameWithoutExtension(Path.GetFileName(symbolReference).Replace(symbolID + "_" + elias.Sprite + "_", ""));
+                            var symbolFileName = Path.GetFileNameWithoutExtension(Path.GetFileName(symbolReference).Replace(symbolID + "_" + Elias.Sprite + "_", ""));
 
-                            var symbolAsset = elias.Assets.FirstOrDefault(asset => asset.FlashAssetName == symbolFileName && this.FlashRectanglePoint[0] == asset.FlashRectanglePoint[0]
-                                && this.FlashRectanglePoint[1] == asset.FlashRectanglePoint[1]);
+                            var symbolAsset = Elias.Assets.FirstOrDefault(asset => asset.FlashAssetName == symbolFileName && FlashRectanglePoint[0] == asset.FlashRectanglePoint[0]
+                                && FlashRectanglePoint[1] == asset.FlashRectanglePoint[1]);
 
                             if (symbolAsset != null)
                             {
                                 Console.WriteLine("Cloned: " + symbolFileName + " => " + FlashAssetName);
 
-                                this.IsMemberAlias = true;
-                                this.IsFlipped = false;
-                                this.FlashSourceAliasName = symbolFileName;
-                                this.ShockwaveSourceAliasName = AssetUtil.ConvertFlashName(this.elias, this.FlashSourceAliasName, elias.X, elias.Y);
+                                IsMemberAlias = true;
+                                IsFlipped = false;
+                                FlashSourceAliasName = symbolFileName;
+                                ShockwaveSourceAliasName = AssetUtil.ConvertFlashName(Elias, FlashSourceAliasName, Elias.X, Elias.Y);
                             }
                             else
                             {
                                 // Copy it over because different regpoints
                                 Console.WriteLine("Copied: " + symbolFileName + " => " + FlashAssetName);
-                                File.Copy(symbolReference, Path.Combine(elias.OUTPUT_PATH, "images", FlashAssetName + ".png"));
+                                File.Copy(symbolReference, Path.Combine(Elias.OUTPUT_PATH, "images", FlashAssetName + ".png"));
                             }
                         }
                         else
@@ -192,7 +192,7 @@ namespace EliasLibrary
                                 Console.WriteLine("Create blank sprite for: " + FlashAssetName);
 
                                 Bitmap bmp = new Bitmap(1, 1);
-                                bmp.Save(Path.Combine(elias.OUTPUT_PATH, "images", FlashAssetName + ".png"), ImageFormat.Png);
+                                bmp.Save(Path.Combine(Elias.OUTPUT_PATH, "images", FlashAssetName + ".png"), ImageFormat.Png);
                             }
                         }
                     }
@@ -211,9 +211,9 @@ namespace EliasLibrary
             if (!string.IsNullOrEmpty(FlashSourceAliasName))
                 return;
 
-            var sourceImage = ImageAssetUtil.SolveFile(elias.OUTPUT_PATH, FlashAssetName);
+            var sourceImage = ImageAssetUtil.SolveFile(Elias.OUTPUT_PATH, FlashAssetName);
             
-            File.Copy(sourceImage, Path.Combine(elias.IMAGE_PATH, this.ShockwaveAssetName + ".png"));
+            File.Copy(sourceImage, Path.Combine(Elias.IMAGE_PATH, ShockwaveAssetName + ".png"));
         }
 
         public void WriteRegPointData()
@@ -228,7 +228,7 @@ namespace EliasLibrary
             if (!string.IsNullOrEmpty(FlashSourceAliasName))
                 return;
 
-            File.WriteAllText(Path.Combine(elias.IMAGE_PATH, this.ShockwaveAssetName + ".txt"), ShockwaveRectanglePoint[0] + "," + ShockwaveRectanglePoint[1]);
+            File.WriteAllText(Path.Combine(Elias.IMAGE_PATH, ShockwaveAssetName + ".txt"), ShockwaveRectanglePoint[0] + "," + ShockwaveRectanglePoint[1]);
         }
     }
 }
